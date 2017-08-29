c46b4a8cb9dc50813d6738f1af9ad105c1905051
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-08-30 00:25:43 +0900

    First version

---
+++ src/index.js
@@ -0,0 +1,36 @@
+let sharedData = {};
+let installed = false;
+
+/**
+ * Add 'shared' option to Vue constructor.
+ */
+function init() {
+  const options = this.$options;
+  if (options.shared) {
+    sharedData = options.shared;
+  }
+}
+
+/**
+ * Install vue-shared-data plugin.
+ *
+ * @param Vue {Vue}
+ */
+export default function install(Vue) {
+  if (installed) {
+    return;
+  }
+
+  Vue.mixin({ beforeCreate: init });
+
+  // Define Vue.$shred property.
+  Object.defineProperty(Vue.prototype, '$shared', { get: () => sharedData });
+
+  installed = true;
+}
+
+
+// When Vue is defined as global, install this plugin automatically.
+if (window && window.Vue && window.Vue.use) {
+  window.Vue.use(install);
+}
